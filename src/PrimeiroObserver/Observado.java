8cb241df06f594ccbf264fb117f3e9bfd7965be8
Mario_junior23 <mariojunior3251@gmail.com>
2020-10-13 04:33:07 -0300

    classe que esta sendo observada

---
+++ src/PrimeiroObserver/Observado.java
@@ -0,0 +1,26 @@
+package PrimeiroObserver;
+
+import java.util.Observable;
+
+ class News extends Observable {
+	
+	void news() {
+		
+		String[] news = {"News 1", "News 1", "New 2"};
+		
+		for(String s: news) {
+			
+			// definindo mudança 
+			setChanged();
+			// notificarObservadores(s);
+			notifyObservers(s);
+			
+			try {
+				Thread.sleep(1000);
+			}catch (InterruptedException e) {
+				System.out.println("Erro foi o corrido");
+			}
+		}
+	}
+  
+}
