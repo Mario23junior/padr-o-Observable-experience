d83efd4b077ae760ef60b9bf05d70500dbf0be26
Mario_junior23 <mariojunior3251@gmail.com>
2020-10-13 04:46:04 -0300

    Executando Observer e Observable

--- src/Observer/Observable.java
+++ src/Observer/Observable.java
@@ -4,5 +4,12 @@ public class Observable {
    
 	public static void main(String[] args) {
 		
+		Observado observerNew = new Observado();
+		ObserverUm leitor1 = new ObserverUm();
+		ObserverDois leitor2 = new ObserverDois();
+		
+		observerNew.addObserver(leitor1);
+		observerNew.addObserver(leitor2);
+		observerNew.news();
  	}
 }
